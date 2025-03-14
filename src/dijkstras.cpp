37a5ff45ca17eb5b8ee31c16f82f6890fe67b0c3
LittleLion930 <masudal@uci.edu>
2025-03-14 05:37:21 +0000

    print_path

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -86,7 +86,6 @@ void print_path(const vector<int>& v, int total) {
         return;
     }
 
-    cout << "Path: ";
     for (size_t i = 0; i < v.size(); ++i) {
         cout << v[i];
         if (i < v.size() - 1) cout << " ";
