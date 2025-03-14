9310c20e3028f8d28d1dc385ece847b8d78b800e
LittleLion930 <masudal@uci.edu>
2025-03-14 06:55:21 +0000

    Added to print an initial newline

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -81,6 +81,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total) {
+    cout << endl; 
     if (v.empty()) {
         cout << "Total cost is " << total << endl;
         return;
